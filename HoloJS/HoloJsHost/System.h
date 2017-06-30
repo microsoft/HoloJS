3933023647cdfc81a4adcccb6b62e0e467490936
Cristi Petruta <gelu@live.com>
2017-06-29 17:01:20 -0700

    Provide a comprehensive timeouts and intervals implementation (#55)
    * Create Timers class to manage timers; better encapsulation of timer definitions
    
    * Add interval timers
    
    * Remove not used method
    
    * Refactor code; add more comments

--- HoloJS/HoloJsHost/System.h
+++ HoloJS/HoloJsHost/System.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <agents.h>
-
 namespace HologramJS
 {
 	namespace API
@@ -9,54 +7,12 @@ namespace HologramJS
 		class System
 		{
 		public:
-			System();
-			~System();
+			System() {}
+			~System() {}
 
 			bool Initialize();
 
 		private:
-
-			class Timeout
-			{
-			public:
-				std::unique_ptr<concurrency::timer<int>> Timer;
-				std::unique_ptr<concurrency::call<int>> Callback;
-				std::unique_ptr<concurrency::task<void>> Continuation;
-				JsValueRef ScriptCallback;
-				std::vector<JsValueRef> ScriptCallbackParameters;
-				int ID;
-			};
-
-			// Lock for the list of active timers
-			std::mutex m_timeoutsLock;
-			
-			// List of active timers
-			// On timer clear or timer firing, it is removed from this list
-			std::list<std::shared_ptr<Timeout>> m_timeouts;
-
-			JsValueRef m_setTimeoutFunction = JS_INVALID_REFERENCE;
-			static JsValueRef CHAKRA_CALLBACK setTimeoutStatic(
-				JsValueRef callee,
-				bool isConstructCall,
-				JsValueRef* arguments,
-				unsigned short argumentCount,
-				PVOID callbackData
-			)
-			{
-				return reinterpret_cast<System*>(callbackData)->setTimeout(callee, arguments, argumentCount);
-			}
-
-			JsValueRef m_clearTimeoutFunction = JS_INVALID_REFERENCE;
-			static JsValueRef CHAKRA_CALLBACK clearTimeoutStatic(
-				JsValueRef callee,
-				bool isConstructCall,
-				JsValueRef* arguments,
-				unsigned short argumentCount,
-				PVOID callbackData
-			)
-			{
-				return reinterpret_cast<System*>(callbackData)->clearTimeout(callee, arguments, argumentCount);
-			}
 
 			JsValueRef m_logFunction = JS_INVALID_REFERENCE;
 			static JsValueRef CHAKRA_CALLBACK logStatic(
@@ -69,18 +25,6 @@ namespace HologramJS
 			{
 				return reinterpret_cast<System*>(callbackData)->log(callee, arguments, argumentCount);
 			}
-
-			JsValueRef setTimeout(
-				JsValueRef callee,
-				JsValueRef* arguments,
-				unsigned short argumentCount
-			);
-
-			JsValueRef clearTimeout(
-				JsValueRef callee,
-				JsValueRef* arguments,
-				unsigned short argumentCount
-			);
 
 			JsValueRef log(
 				JsValueRef callee,
