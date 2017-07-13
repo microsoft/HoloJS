8609d534183f968cd621af3f814f4381c824fff9
Cristian Petruta <crispet@ntdev.microsoft.com>
2017-07-13 07:23:17 -0700

    Move error handling macros to their own header

--- HoloJS/HoloJsHost/pch.h
+++ HoloJS/HoloJsHost/pch.h
@@ -23,9 +23,6 @@
 #include <EGL/eglext.h>
 #include <EGL/eglplatform.h>
 
-// ANGLE include for Windows Store
-#include <angle_windowsstore.h>
-
 //WIC
 #include <wincodec.h>
 
@@ -37,25 +34,4 @@
 //D3D
 #include <d3d11.h>
 
-void Log(PCSTR file, int line);
-
-#define EXIT_IF_JS_ERROR(x) do { if ((x) != JsNoError) { Log(__FILE__, __LINE__); return; } } while (0, 0)
-#define EXIT_IF_FALSE(x) do { if ((x) == false) { Log(__FILE__, __LINE__); return; } } while (0, 0)
-#define EXIT_IF_TRUE(x) do { if ((x) == true) { Log(__FILE__, __LINE__); return; } } while (0, 0)
-#define EXIT_IF_FAILED(x) do { if (FAILED(x)) { Log(__FILE__, __LINE__); return; } } while (0, 0)
-#define RETURN_IF_JS_ERROR(x) do { if ((x) != JsNoError) { Log(__FILE__, __LINE__); return false; } } while (0, 0)
-#define RETURN_IF_FAILED(x) do { if (FAILED(x)) { Log(__FILE__, __LINE__); return false; } } while (0, 0)
-#define RETURN_NULL_IF_JS_ERROR(x) do { if ((x) != JsNoError) { Log(__FILE__, __LINE__); return nullptr; } } while (0, 0)
-#define RETURN_IF_FALSE(x) do { if ((x) == false) { Log(__FILE__, __LINE__); return false; } } while (0, 0)
-#define RETURN_IF_TRUE(x) do { if ((x) == true) { Log(__FILE__, __LINE__); return false; } } while (0, 0)
-#define RETURN_IF_NULL(x) do { if ((x) == nullptr) { Log(__FILE__, __LINE__); return false; } } while (0, 0)
-#define RETURN_NULL_IF_FALSE(x) do { if ((x) == false) { Log(__FILE__, __LINE__); return nullptr; } } while (0, 0)
-#define RETURN_NULL_IF_FAILED(x) do { if (FAILED(x)) { Log(__FILE__, __LINE__); return nullptr; } } while (0, 0)
-#define RETURN_INVALID_REF_IF_FALSE(x) do { if ((x) == false) { Log(__FILE__, __LINE__); return JS_INVALID_REFERENCE; } } while (0, 0)
-#define RETURN_INVALID_REF_IF_TRUE(x) do { if ((x) == true) { Log(__FILE__, __LINE__); return JS_INVALID_REFERENCE; } } while (0, 0)
-#define RETURN_INVALID_REF_IF_FAILED(x) do { if (FAILED(x)) { Log(__FILE__, __LINE__); return JS_INVALID_REFERENCE; } } while (0, 0)
-#define RETURN_INVALID_REF_IF_NULL(x) do { if ((x) == nullptr) { Log(__FILE__, __LINE__); return JS_INVALID_REFERENCE; } } while (0, 0)
-#define RETURN_INVALID_REF_IF_JS_ERROR(x) do { if ((x) != JsNoError) { Log(__FILE__, __LINE__); return JS_INVALID_REFERENCE; } } while (0, 0)
-
-#define HANDLE_EXCEPTION_IF_JS_ERROR(x) if ((x) != JsNoError) { HologramJS::ScriptErrorHandling::PrintException();}
-#define RETURN_ON_SCRIPT_ERROR(x) if ((x) != JsNoError) { HologramJS::ScriptErrorHandling::PrintException(); return false; }
+#include "ErrorHandling.h"
