88c5e4c10af204d8ab1b0f0392a73e5032c84d7c
Stuart Anderson <stuart.anderson@data61.csiro.au>
2017-09-02 09:10:03 +1000

    Upgrade angle and render using projection matrix

--- HoloJS/ThreeJSApp/Scripts/HolographicCamera.js
+++ HoloJS/ThreeJSApp/Scripts/HolographicCamera.js
@@ -1,6 +1,6 @@
 THREE.HolographicCamera = class HolographicCamera extends THREE.Camera {
 
-    constructor() {
+    constructor () {
         super();
 
         this.type = 'HolographicCamera';
@@ -9,42 +9,21 @@ THREE.HolographicCamera = class HolographicCamera extends THREE.Camera {
         this._holographicViewMatrix = new THREE.Matrix4();
         this._holographicTransformMatrix = new THREE.Matrix4();
         this._holographicProjectionMatrix = new THREE.Matrix4();
-        this._flipMatrix = new THREE.Matrix4().makeScale(-1, 1, 1);
-        this._identityProjectionMatrix = new THREE.Matrix4();
-
-        this._prerender = new THREE.Mesh(new THREE.BufferGeometry(), new THREE.MeshBasicMaterial());
-        this._prerender.frustumCulled = false;
-        this._prerender.renderOrder = Number.NEGATIVE_INFINITY;
-
-        this._postrender = new THREE.Mesh(new THREE.BufferGeometry(), new THREE.MeshBasicMaterial());
-        this._postrender.frustumCulled = false;
-        this._postrender.renderOrder = Number.POSITIVE_INFINITY;
-
-        this._prerender.onBeforeRender = (renderer, scene, camera, geometry, material, group) => {
-            camera.projectionMatrix.copy(this._identityProjectionMatrix);
-        };
-
-        this._postrender.onAfterRender = (renderer, scene, camera, geometry, material, group) => {
-            camera.projectionMatrix.copy(this._holographicProjectionMatrix);
-        };
-
-        this.add(this._prerender);
-        this.add(this._postrender);
     }
 
-    update() {
+    update () {
         if (!window.experimentalHolographic) return;
 
         let params = window.getHolographicCameraParameters();
 
         // view matrix
-        this._holographicViewMatrix.fromArray(params.mid.viewMatrix);
-        this._holographicViewMatrix.multiply(this._flipMatrix); // flip (TODO: Why is this required?)
+        this._holographicViewMatrix.fromArray(params.mid.viewMatrix); // store
         this._holographicTransformMatrix.getInverse(this._holographicViewMatrix); // invert
         this._holographicTransformMatrix.decompose(this.position, this.quaternion, this.scale); // decompose
 
-        // projection matrix (used for culling only - do not use to render)
-        this._holographicProjectionMatrix.fromArray(params.mid.projectionMatrix);
-        this.projectionMatrix.copy(this._holographicProjectionMatrix);
+        // projection matrix
+        this._holographicProjectionMatrix.fromArray(params.mid.projectionMatrix); // store
+        this.projectionMatrix.copy(this._holographicProjectionMatrix); // copy
+        // TODO: Extend PerspectiveCamera and decompose fov, aspect, near, far values
     }
 };
