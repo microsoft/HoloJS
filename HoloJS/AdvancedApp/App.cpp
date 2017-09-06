dd8305b0ec02dcdbcfe73d7c4ea1db2e955c1dfc
Stuart Anderson <stuart.anderson@data61.csiro.au>
2017-09-06 16:41:20 +1000

    Add sample app for advanced mode

---
+++ HoloJS/AdvancedApp/App.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <WindowsNumerics.h>
+
+using namespace Windows::ApplicationModel::Core;
+using namespace Platform;
+using namespace HologramJS;
+
+// The main function creates an IFrameworkViewSource for our app, and runs the app.
+[Platform::MTAThread]
+int main(Array<Platform::String^>^)
+{
+    auto holoJsAppView= ref new HoloJsAppView(ref new String(L"scripts/app.json"));
+
+    // use advanced mode rendering
+    holoJsAppView->AutoStereoEnabled = false;
+
+    auto holoJsAppSource = ref new HoloJsAppSource(holoJsAppView);
+    CoreApplication::Run(holoJsAppSource);
+    return 0;
+}
