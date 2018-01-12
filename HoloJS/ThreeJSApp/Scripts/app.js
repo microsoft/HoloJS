c157d6ffe68850536e90b706db746edf88e3abcc
Cristian Petruta <crispet@microsoft.com>
2018-01-11 23:10:43 -0800

    Convert ThreeJS samples to use WebVR style stereo rendering.

--- HoloJS/ThreeJSApp/Scripts/app.js
+++ HoloJS/ThreeJSApp/Scripts/app.js
@@ -9,15 +9,17 @@ if (!isHoloJs) {
 
 let renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 let scene = new THREE.Scene();
-let camera = (isHoloJs && holographic.renderMode > 0 ? new THREE.HolographicCamera() : new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000));
+
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 1000);
+scene.add(camera);
+
 let clock = new THREE.Clock();
 
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-scene.add(camera); // this is required for HolographicCamera to function correctly
 
 var controls;
 
+// Not running in HoloJS or rendering flat
 if (!isHoloJs || holographic.renderMode === 0) {
     camera.position.set(0, 0, 1);
     controls = new THREE.OrbitControls(camera, canvas);
@@ -25,8 +27,6 @@ if (!isHoloJs || holographic.renderMode === 0) {
 
 function update(delta, elapsed) {
     window.requestAnimationFrame(() => update(clock.getDelta(), clock.getElapsedTime()));
-
-    if (camera.update) camera.update();
 
     if (sample.update) sample.update(delta, elapsed);
 
@@ -45,12 +45,19 @@ function onVoiceCommand(voiceEvent) {
     console.log("Voice command: " + voiceEvent.command + "; confidence: " + voiceEvent.confidence);
 }
 
+renderer.vr.enabled = true;
+if (isHoloJs) {
+    holographic.getVRDisplays().then(function (value) { renderer.vr.setDevice(value[0]); });
+} else {
+    navigator1.getVRDisplays().then(function (value) { renderer.vr.setDevice(value[0]); });
+}
+
 // The samples are usually mutually exclusive; enable them one at a time
 
-//var sample = new ShadowsExample(scene, renderer);
+var sample = new ShadowsExample(scene, renderer);
 //var sample = new BasicCubeExample(scene, renderer);
 //var sample = new CanvasRenderingExample(scene, renderer);
-var sample = new LightsExample(scene, renderer);
+//var sample = new LightsExample(scene, renderer);
 //var surfaceReconstructionExample = new SurfaceReconstructionExample(scene, renderer);
 
 start();
