c9d5d481f8b8dcdb9e49585b11c72078b8770b70
Cristian Petruta <crispet@microsoft.com>
2018-01-19 22:41:05 -0800

    Update sample to render received chat messages

--- HoloJS/ThreeJSApp/Scripts/chat.js
+++ HoloJS/ThreeJSApp/Scripts/chat.js
@@ -1,22 +1,55 @@
+// Chat client for the socket.io chat sample at https://socket.io/demos/chat/
 function ChatWebsocketExample(scene, renderer) {
-
     var url = "https://socket-io-chat.now.sh";
-    var socket = io(url, {transports: ['websocket']});
+    var socket = io(url, { transports: ['websocket'] });
 
-    // Whenever the server emits 'new message', update the chat body
+    this.scene = scene;
+    this.material = new THREE.MeshStandardMaterial({ vertexColors: THREE.VertexColors });
+    scene.add(new THREE.AmbientLight(0xFFFFFF, 0.8));
+
+    // Load fonts
+    let fontLoader = new THREE.FontLoader();
+    fontLoader.load('helvetiker_regular.typeface.json', function (font) {
+        this.font = font;
+    }.bind(this)); //end load function
+
+    // Whenever the server emits 'new message', render the text
     socket.on('new message', function (data) {
-        console.log(data.username + ":" + data.message);
-    });
+        console.log(data.username + " : " + data.message);
 
-    // Whenever the server emits 'user joined', log it in the chat body
+        // Remove previous message
+        if (this.textMesh) {
+            scene.remove(this.textMesh);
+        }
+
+        // If we have a font loaded, show the message
+        if (this.font) {
+            var textGeometry = new THREE.TextGeometry(data.username + ":" + data.message, {
+                font: this.font,
+                size: 0.2,
+                height: 0.05,
+                curveSegments: 12
+            });
+
+            this.textMesh = new THREE.Mesh(textGeometry, this.material);
+            this.textMesh.position.z = -3;
+            this.textMesh.position.x = -2;
+            this.scene.add(this.textMesh);
+        }
+
+    }.bind(this));
+
+    // Whenever the server emits 'user joined', log it
     socket.on('user joined', function (data) {
         console.log(data.username + ' joined');
     });
 
+    // Join the chat
     socket.emit('add user', 'holojs-test');
 
+    // Send a new message
+    socket.emit('new message', "This is a new message");
 
-    this.update = function (delta, elapsed) {
-        
+    this.update = function (delta, elapsed) {    
     };
 }
